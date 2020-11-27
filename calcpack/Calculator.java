97e57a1cf3ddbbc6072c3f0f0e14d0139a97e0fd
Phil Fernandez <norcal618@gmail.com>
2020-11-27 00:23:12 -0800

    catch exception

--- calcpack/Calculator.java
+++ calcpack/Calculator.java
@@ -1,4 +1,5 @@
 package calcpack;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -7,49 +8,50 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        float operand1, operand2;
-        char choice;
+        float operand1 = 0, operand2 = 0;
+        int choice;
         Scanner in = new Scanner(System.in);
         do {
-            System.out.print("(1) Addition\n");
-            System.out.print("(2) Subtraction\n");
-            System.out.print("(3) Multiplication\n");
-            System.out.print("(4) Division\n");
-            System.out.print("(5) Exit\n\n");
-            System.out.print("Enter Your Choice : ");
-            choice = in.next().charAt(0);
-            switch(choice) {
-                case '1':
+            System.out.println("(1) Addition");
+            System.out.println("(2) Subtraction");
+            System.out.println("(3) Multiplication");
+            System.out.println("(4) Division");
+            System.out.println("(5) Exit");
+            System.out.print("\nEnter Your Choice : ");
+            choice = in.nextInt();
+
+            if (choice == 1 || choice == 2 || choice == 3 || choice == 4) {
+                try {
                     System.out.print("Enter two numbers : ");
                     operand1 = in.nextFloat();
                     operand2 = in.nextFloat();
+
+                } catch(InputMismatchException e) {
+                    System.out.println("Non-Digit character detected : Program will terminate");
+                    System.exit(1);
+                }
+            }
+
+            switch(choice) {
+                case 1:
                     System.out.printf("\n%.2f + %.2f = %.2f\n\n", operand1, operand2, operand1+operand2);
                     break;
-                case '2':
-                    System.out.print("Enter two numbers : ");
-                    operand1 = in.nextFloat();
-                    operand2 = in.nextFloat();
+                case 2:
                     System.out.printf("\n%.2f - %.2f = %.2f\n\n", operand1, operand2, operand1-operand2);
                     break;
-                case '3':
-                    System.out.print("Enter two numbers : ");
-                    operand1 = in.nextFloat();
-                    operand2 = in.nextFloat();
+                case 3:
                     System.out.printf("\n%.2f * %.2f = %.2f\n\n", operand1, operand2, operand1*operand2);
                     break;
-                case '4':
-                    System.out.print("Enter two numbers : ");
-                    operand1 = in.nextFloat();
-                    operand2 = in.nextFloat();
+                case 4:
                     System.out.printf("\n%.2f / %.2f = %.2f\n\n", operand1, operand2, operand1/operand2);
                     break;
-                case '5':
+                case 5:
                     System.out.println("Goodbye...");
                     System.exit(0);
                 default:
                     System.out.println("Make a valid choice (1 - 5)\n");
             }
-        } while(choice != '5');
+        } while(choice != 5);
 
         in.close();
     }
